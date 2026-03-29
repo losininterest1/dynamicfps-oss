cb764f2210c71d2bef52b5e7de8a719ae0c71f3f
aaaaaaaaa <yurogami48@gmail.com>
2026-03-29 23:41:28 +0800

    Update DynamicFPS.cpp

--- src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.cpp
+++ src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.cpp
@@ -1,1 +1,33 @@
+#include "DynamicFPS.hpp"
+#include "../../Client.h"
+#include <Windows.h>
 
+void DynamicFPS::onTick() {
+    if (!enabled.boolValue) return;
+
+    auto mc = Client::getMinecraft();
+    auto gameSettings = mc->getOptions();
+    
+    HWND minecraftWindow = FindWindowA("LWJGL", nullptr);
+    bool isFocused = (GetForegroundWindow() == minecraftWindow); 
+    
+    if (!isFocused) {
+        gameSettings->framerateLimit = (int)unfocusedFPS.floatValue;
+    } 
+    else if (isAFK()) {
+        gameSettings->framerateLimit = (int)afkFPS.floatValue;
+    } 
+    else {
+        gameSettings->framerateLimit = 165; 
+    }
+}
+
+bool DynamicFPS::isAFK() {
+    LASTINPUTINFO lii;
+    lii.cbSize = sizeof(LASTINPUTINFO);
+    if (GetLastInputInfo(&lii)) {
+        uint64_t idleTime = (GetTickCount64() - lii.dwTime) / 1000;
+        return idleTime >= (uint64_t)afkTimeout.floatValue;
+    }
+    return false;
+}
