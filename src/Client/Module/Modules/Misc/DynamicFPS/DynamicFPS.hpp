12ac53a9f1f23c9902783af2906b1e2c4322c625
aaaaaaaaa <yurogami48@gmail.com>
2026-03-30 00:47:33 +0800

    Update DynamicFPS.hpp

--- src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.hpp
+++ src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.hpp
@@ -1,9 +1,12 @@
+// 12:47 UTC+8 Leqixn
+
 #pragma once
 #include "../Module.h"
+#include <optional>
 
 class DynamicFPS : public Module {
 public:
-    DynamicFPS() : Module("DynamicFPS", "Misc", "Lowers FPS when AFK or tabbed out") {
+    DynamicFPS() : Module("DynamicFPS", "Misc", "Reduces the FPS on your game while afk or on other tab") {
         addSetting(&enabled);
         addSetting(&unfocusedFPS);
         addSetting(&afkFPS);
@@ -11,13 +14,17 @@ public:
     }
 
     Setting enabled = Setting("Enabled", true);
-    Setting unfocusedFPS = Setting("Unfocused FPS", 20.0f, 5.0f, 60.0f);
-    Setting afkFPS = Setting("AFK FPS", 30.0f, 5.0f, 60.0f);
-    Setting afkTimeout = Setting("AFK Timeout (Sec)", 60.0f, 10.0f, 300.0f);
+    Setting unfocusedFPS = Setting("Unfocused FPS", 20.0f, 1.0f, 60.0f);
+    Setting afkFPS = Setting("AFK FPS", 30.0f, 1.0f, 60.0f);
+    Setting afkTimeout = Setting("AFK Timeout", 60.0f, 5.0f, 300.0f);
 
     void onTick() override;
-    bool isAFK();
-    
+    void onDisable() override;
+
 private:
-    int originalLimit = 0;
+    bool isAFK() const;
+    void setLimit(int fps);
+    
+    std::optional<int> originalLimit;
+    bool isThrottled = false;
 };
