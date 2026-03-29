e6712327508098a62010e0d257c95f19e982dea0
aaaaaaaaa <yurogami48@gmail.com>
2026-03-30 01:52:59 +0800

    Update DynamicFPS.cpp

--- src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.cpp
+++ src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.cpp
@@ -1,7 +1,7 @@
-// Updated 1:20 UTC+8 by Leqixn
+// Updated 1:52 UTC+8 by Leqixn
 
 #include "DynamicFPS.hpp"
-#include "SDK/SDK.hpp"
+#include "src/SDK/SDK.hpp"
 #include <Windows.h>
 
 void DynamicFPS::onTick() {
