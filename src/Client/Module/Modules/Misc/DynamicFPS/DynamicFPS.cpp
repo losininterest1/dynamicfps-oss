a22f0051bd41757beb7d54289bd38c25713ef73a
aaaaaaaaa <yurogami48@gmail.com>
2026-03-30 00:45:17 +0800

    Update DynamicFPS.cpp

--- src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.cpp
+++ src/Client/Module/Modules/Misc/DynamicFPS/DynamicFPS.cpp
@@ -1,33 +1,54 @@
+// 12:44 UTC+8 Leqixn
+
 #include "DynamicFPS.hpp"
-#include "../../Client.h"
+#include "../../../SDK/Minecraft.h"
 #include <Windows.h>
 
 void DynamicFPS::onTick() {
     if (!enabled.boolValue) return;
 
-    auto mc = Client::getMinecraft();
-    auto gameSettings = mc->getOptions();
+    auto instance = Minecraft::getClientInstance();
+    if (!instance || !instance->getOptions()) return;
+
+    auto options = instance->getOptions();
+
+    HWND foreground = GetForegroundWindow();
+    char className[256];
+    GetClassNameA(foreground, className, sizeof(className));
     
-    HWND minecraftWindow = FindWindowA("LWJGL", nullptr);
-    bool isFocused = (GetForegroundWindow() == minecraftWindow); 
-    
-    if (!isFocused) {
-        gameSettings->framerateLimit = (int)unfocusedFPS.floatValue;
+    bool isTabbedOut = (strcmp(className, "ApplicationFrameWindow") != 0 && 
+                        strcmp(className, "Windows.UI.Core.CoreWindow") != 0);
+
+    if (isTabbedOut || isAFK()) {
+        if (!isThrottled) {
+            originalLimit = options->framerateLimit;
+            isThrottled = true;
+        }
+        options->framerateLimit = isTabbedOut ? (int)unfocusedFPS.floatValue : (int)afkFPS.floatValue;
     } 
-    else if (isAFK()) {
-        gameSettings->framerateLimit = (int)afkFPS.floatValue;
-    } 
-    else {
-        gameSettings->framerateLimit = 165; 
+    else if (isThrottled) {
+        if (originalLimit.has_value()) {
+            options->framerateLimit = originalLimit.value();
+        }
+        isThrottled = false;
     }
 }
 
-bool DynamicFPS::isAFK() {
-    LASTINPUTINFO lii;
-    lii.cbSize = sizeof(LASTINPUTINFO);
+bool DynamicFPS::isAFK() const {
+    LASTINPUTINFO lii = { sizeof(LASTINPUTINFO) };
     if (GetLastInputInfo(&lii)) {
         uint64_t idleTime = (GetTickCount64() - lii.dwTime) / 1000;
         return idleTime >= (uint64_t)afkTimeout.floatValue;
     }
     return false;
+}
+
+void DynamicFPS::onDisable() {
+    if (isThrottled && originalLimit.has_value()) {
+        auto instance = Minecraft::getClientInstance();
+        if (instance && instance->getOptions()) {
+            instance->getOptions()->framerateLimit = originalLimit.value();
+        }
+    }
+    isThrottled = false;
 }
